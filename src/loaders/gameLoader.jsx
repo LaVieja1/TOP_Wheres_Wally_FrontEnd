411c1fad8ce17b540f7d746012624e2872e904a8
santicano <canosantiago404@gmail.com>
2024-01-14 11:30:00 -0300

    añadido gameLoader para fetch la data de los juegos

---
+++ src/loaders/gameLoader.jsx
@@ -0,0 +1,15 @@
+const gameLoader = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/api/games');
+        if (!response.ok) {
+            throw new Error(`Esto es un error HTTP: El status es ${response.status}`);
+        }
+
+        const games = await response.json();
+        return { games };
+    } catch (err) {
+        throw new Error('No fue posible conseguir la data con la operación fetch');
+    }
+}
+
+export default gameLoader;
