64b9e836391363aa87edc58e82fd78cee99d2009
santicano <canosantiago404@gmail.com>
2024-01-14 14:32:55 -0300

    añadido media queries al css del header

--- src/components/GameTimer.jsx
+++ src/components/GameTimer.jsx
@@ -1,3 +1,5 @@
+import './styles/GameTimer.css';
+
 export default function GameTimer({ gameTimer }) {
     return <div className="game-timer">{new Date(gameTimer * 1000).toISOString().slice(11, 19)}</div>
 }
